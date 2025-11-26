e0b082dca063fb0e051379c0c067b02f3317375d
IdrissiAymen <gilnashidrissi@gmail.com>
2025-11-26 09:54:42 +0100

    Initial commit

---
+++ src/main/java/net/youssfi/webmvcproducts/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package net.youssfi.webmvcproducts.repository;
+
+import net.youssfi.webmvcproducts.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository <Product,Long> {
+}
